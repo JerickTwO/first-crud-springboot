19e24261ae7d39b3ab274322c90a4a9fa851990a
JerickTwO <jerickjjtm774@gmail.com>
2024-07-02 06:52:36 -0500

    Create a class for Students

---
+++ src/main/java/dev/jerick/api/entity/Student.java
@@ -0,0 +1,31 @@
+package dev.jerick.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "tbl_student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+
+    private String FirstName;
+
+    private String lastName;
+
+    @Column(name = "email_address",unique = true)
+    private String email;
+}
+
+
+
+
+
+
+
+
+
+

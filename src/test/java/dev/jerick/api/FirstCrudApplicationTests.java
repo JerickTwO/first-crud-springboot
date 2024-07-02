e87e85cc3fc29a876949464eb11aff23fe702593
JerickTwO <jerickjjtm774@gmail.com>
2024-07-01 22:20:21 -0500

    first commit

---
+++ src/test/java/dev/jerick/api/FirstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jerick.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
